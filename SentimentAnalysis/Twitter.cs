d8433e9d44bc3afb6523cdb73bf3c26a00cb55b4
Unknown <catan85@gmail.com>
2018-03-27 19:50:11 +0200

    initial commit

---
+++ SentimentAnalysis/Twitter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SentimentAnalysis
+{
+    class Twitter
+    {
+        private string access_token;
+        public void Authorize(string credentials)
+        {
+            var post = WebRequest.Create("https://api.twitter.com/oauth2/token") as HttpWebRequest;
+            post.Method = "POST";
+            post.ContentType = "application/x-www-form-urlencoded";
+            post.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
+            var reqbody = Encoding.UTF8.GetBytes("grant_type=client_credentials");
+            post.ContentLength = reqbody.Length;
+            using (var req = post.GetRequestStream())
+            {
+                req.Write(reqbody, 0, reqbody.Length);
+            }
+            try
+            {
+                string respbody = null;
+                using (var resp = post.GetResponse().GetResponseStream())//there request sends
+                {
+                    var respR = new StreamReader(resp);
+                    respbody = respR.ReadToEnd();
+                }
+                //TODO use a library to parse json
+                access_token = respbody.Substring(respbody.IndexOf("access_token\":\"") + "access_token\":\"".Length, respbody.IndexOf("\"}") - (respbody.IndexOf("access_token\":\"") + "access_token\":\"".Length));
+            }
+            catch //if credentials are not valid (403 error)
+            {
+                //TODO
+            }
+        }
+
+        public string Request(string url, string filePath)
+        {
+            var gettimeline = WebRequest.Create(url) as HttpWebRequest;
+            gettimeline.Method = "GET";
+            gettimeline.Headers[HttpRequestHeader.Authorization] = "Bearer " + access_token;
+            try
+            {
+                string respbody = null;
+                using (var resp = gettimeline.GetResponse().GetResponseStream())//there request sends
+                {
+                    var respR = new StreamReader(resp);
+                    respbody = respR.ReadToEnd();
+                }
+
+                //TODO use a library to parse json
+                
+                System.IO.File.WriteAllText(filePath, respbody);
+                return respbody;
+            }
+            catch //401 (access token invalid or expired)
+            {
+                return "ERROR";
+            }
+        }
+
+    }
+}
